c4cbb3790d4848128cab48d2ae70810d83c4a28c
Sharon Chatragadda <54977773+SecretAgentShh@users.noreply.github.com>
2024-01-21 09:31:39 -0500

    Flashcard creation along with main blanked image
    Create flashcard images and save to a different folder

--- matlabdetection.m
+++ matlabdetection.m
@@ -1,18 +1,22 @@
 % Get an image from textbook or photo
 I = imread("C:\Users\sharo\Desktop\aheart.jpg");
-oparray = [];
+% use functionality of computer vision toolbox to detect text
 boundary_box = detectTextCRAFT(I);
+% print boundary box array
 disp(boundary_box);
+% disp(boundary_box(3,:));
+%Print the blank over text field and display masked image
 Iout = insertShape(I,"filled-rectangle",boundary_box,LineWidth=3,ShapeColor="black",Opacity=1);
+%show figure ina  separate window
 figure
 imshow(Iout)
-position = floor([(boundary_box(:,1)+boundary_box(:,3))/2,(boundary_box(:,2)+boundary_box(:,4))/2]) 
-temp = sum(position');
-[val, idx] = sort(temp);
-newpositions = position(idx,:);
-RGB = insertText(Iout, newpositions(:,1:2), 1:size(newpositions,1), 'FontSize', 22, 'AnchorPoint' ,'LeftBottom');
-% Assign numbers to each bb
-figure
-%imshow(RGB);
 
-imwrite(RGB,"C:\newfolderie");
+% save image to a different destination
+imwrite(Iout,"C:\newfolderie\pic.jpg");
+
+inti = 1;
+for valu = 1: numel(boundary_box)/4
+    Ifi = insertShape(I,"filled-rectangle",boundary_box(valu,:),LineWidth=3,ShapeColor="black",Opacity=1);
+    inti = inti + 1;
+    imwrite(Ifi,"C:\newfolderie\"+inti+".jpg");
+end
