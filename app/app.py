d88f74fe5c6f8ce3cfd68d617520bf4b8230c337
Jie Ying Huang <124211458+jieyingh@users.noreply.github.com>
2024-01-21 07:23:15 -0500

    UI
    made progress on UI

--- app/app.py
+++ app/app.py
@@ -3,23 +3,35 @@ from shiny import App, render, ui, reactive
 app_ui = ui.page_fillable(
     ui.row(
         ui.column(12, 
-            ui.card("Width 12",
+            ui.card("Outer",
                 ui.row(
                     ui.column(3,
-                        ui.card("Width 3",
+                        ui.card("Navbar",
                             ui.row(
-                                ui.column(12, ui.card("Width 12")),
+                                ui.column(12, ui.card("probably logo")),
                                 ui.column(12, ui.card(
+                                    # description or instructions
+                                    ui.markdown("""
+                                                Make memorizing diagrams or maps a breeze!
+                                                
+                                                Simply upload an image in .png, .jpeg, or .jpg format and start quizzing yourself
+                                                """),
+                                    # lets user upload files
                                     ui.input_file("inputImage", "Choose your diagram!", accept=[".png", ".jpeg", ".jpg"], multiple=False,
-                                                  button_label='Browse...', placeholder='None chosen', capture=None))
+                                                  button_label='Browse...', placeholder='None chosen', capture=None),
+                                    # used to submit, if not functions will run regardless
+                                    ui.input_action_button("submit", "Submit", class_="btn-primary")
+                                    )
+                                    
                                     )
                             )
                         )
                     ),
                     ui.column(9,
-                              ui.card("width 9"),
-                              ui.card("width 9"),
-                              ui.card("width 9")
+                              ui.card("top display", 
+                                      ui.output_text_verbatim("printout")),
+                              ui.card("main image"),
+                              ui.card("navigation buttons")
                               )
                                 
                 )
@@ -32,8 +44,17 @@ app_ui = ui.page_fillable(
 def server(input, output, session):
     @reactive.Calc
     def pathToMathlab():
-        path = input.inputImage()[0].get("datapath")
-        return path
+        while type(input.inputImage) != 'NoneType':
+            path = input.inputImage()[0].get("datapath")
+            return path
+        else:
+            return "not done yet"
+    
+    @output
+    @render.text
+    def printout():
+        return input.inputImage()[0].get("datapath")
+        #return f'the temp path of the image is {pathToMathlab()}'
         
 
 
